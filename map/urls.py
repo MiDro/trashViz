4910346651c25c66f8005eed4111cd866be2a95a
hmc-cs-ewhitsett <ewhitsett@g.hmc.edu>
2017-06-08 16:52:37 -0700

    Fixed merge conflicts

--- map/urls.py
+++ map/urls.py
@@ -3,14 +3,11 @@ from . import views
 
 app_name = 'map'
 urlpatterns = [
-<<<<<<< HEAD
     url(r'^$', views.index, name='index'),
     url(r'(?P<trashCan_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<trashCan_id>[0-9]+)/$', views.detail, name='detail'),
-=======
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', 
-    	views.DetailView.as_view(), 
+    url(r'^(?P<pk>[0-9]+)/$',
+    	views.DetailView.as_view(),
     	name='detail'),
->>>>>>> 7512e97dbbebffe9bd6930fc0f5d29550fe116ec
 ]
