7512e97dbbebffe9bd6930fc0f5d29550fe116ec
Herrick Fang <herrickfang@gmail.com>
2017-06-08 16:22:48 -0700

    finished basic tutorial

--- map/urls.py
+++ map/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'map'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<trashCan_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', 
+    	views.DetailView.as_view(), 
+    	name='detail'),
 ]
--- map/views.py
+++ map/views.py
@@ -1,16 +1,32 @@
 from django.http import HttpResponse, Http404
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import TrashCan
 
-def index(request):
-    latest_trash_list = TrashCan.objects.order_by('-name')
-    template = loader.get_template('map/index.html')
-    context = {
-        'latest_trash_list' : latest_trash_list,
-    }
-    return HttpResponse(template.render(context, request))
 
-def detail(request, trashCan_id):
-    trashCan = get_object_or_404(TrashCan, pk=trashCan_id)
-    return render(request, 'map/detail.html', {'TrashCan': trashCan})
+
+class IndexView(generic.ListView):
+    template_name = 'map/index.html'
+    context_object_name = 'latest_trash_list'
+
+    def get_queryset(self):
+        """ Return the last five published questions. """
+        return TrashCan.objects.order_by('-name')[:5]
+
+# def index(request):
+#     latest_trash_list = TrashCan.objects.order_by('-name')
+#     template = loader.get_template('map/index.html')
+#     context = {
+#         'latest_trash_list' : latest_trash_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# def detail(request, pk):
+#     trashCan = get_object_or_404(TrashCan, pk=pk)
+#     return render(request, 'map/detail.html', {'trashcan': trashCan})
+
+class DetailView(generic.DetailView):
+    template_name = 'map/detail.html'
+    model = TrashCan
+    
