b3e51d579c1a118fd5442aaf5a0f23e7d14ae0d1
hmc-cs-ewhitsett <ewhitsett@g.hmc.edu>
2017-06-09 13:25:42 -0700

    Fixing trash display

--- map/urls.py
+++ map/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'map'
 urlpatterns = [
-    url(r'/map/$', views.MapView.as_view(), name='map'),
+    url(r'map/$', views.MapView.as_view(), name='map'),
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
