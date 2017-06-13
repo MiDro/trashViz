7b01aed1bcbafffda696c993e1f6c6707cbfd876
Herrick Fang <herrickfang@gmail.com>
2017-06-13 15:06:54 -0700

    more modular links

--- map/templates/map/map.js
+++ map/templates/map/map.js
@@ -29,10 +29,13 @@ for(i=0; i < 40; i++){
 }
 var i;
 
-cols = ['#BF0014','#c66100', '#ca9f00', '#ca9f00', '#bbce00', '#80d200', '#42d500', '#02d900']
-for(i = 0; i < fills.length; i++){
+var cols = ['#BF0014','#c66100', '#ca9f00', '#ca9f00', '#bbce00', '#80d200', '#42d500', '#02d900']
 
-    var colVal = 100 - parseFloat(fills[i]);
+var count = 0;
+
+{% for bin in latest_trash_list %}
+    count += 1;
+    var colVal = 100 - parseFloat(fills[count-1]);
     if(colVal < 20){
         col = cols[0];
     }else if(colVal < 25){
@@ -49,27 +52,28 @@ for(i = 0; i < fills.length; i++){
         col = cols[6];
     }
 
-    console.log(fills);
     //Make an SVG Container
     var svgContainer = d3.select("#trashDisplays").append("svg")
         .attr("width", 100)
         .attr("height", 150)
         .attr("padding", "10px 10px 10px 10px");
     //Draw the Rectangle
-    svgContainer.append("rect")
+    svgContainer.append("a")
+        .attr("href", "{% url 'map:detail' bin.id %}")
+        .append("rect")
         .attr("width", 50)
         .attr("x", 25)
         .attr("y", function(d){
-            return 110 - parseFloat(fills[i]);
+            return 110 - parseFloat(fills[count-1]);
         })
         .attr("height", function(d){
-            return parseFloat(fills[i]) + 10;})
+            return parseFloat(fills[count-1]) + 10;})
         .attr("fill", col);
-
+    //Append text
     svgContainer.append("text")
-        .attr('class', 'TrashCan' + i)
+        .attr('class', 'TrashCan' + count)
         .attr('text-anchor', 'middle')
         .attr("x", 50)
         .attr("y", 10)
-        .text('#' + (i+1));
-}
+        .text('#' + count);
+{% endfor %}
