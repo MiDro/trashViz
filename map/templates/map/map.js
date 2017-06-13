f1be915d493debd115489d8dcda9b921bbc0b8e1
Herrick Fang <herrickfang@gmail.com>
2017-06-13 12:31:59 -0700

    ugly graph updated

--- map/templates/map/map.js
+++ map/templates/map/map.js
@@ -23,42 +23,54 @@ function initMap() {
 
  // get the fills
  var fills = [{% for bin in latest_trash_list %} "{{bin.fillLevel}}", {% endfor %}];
-
+ fills = [];
+ for(i=0; i < 40; i++){
+  fills.push(Math.random()*100);
+ }
  var i;
+
+ cols = ['#BF0014','#c66100', '#ca9f00', '#ca9f00', '#bbce00', '#80d200', '#42d500', '#02d900']
  for(i = 0; i < fills.length; i++){
-  var col;
+  
   var colVal = 100 - parseFloat(fills[i]);
-  if(colVal < 40){
-    col = '#800000'
-  }else if(colVal< 60){
-    col = '#58BF00'; 
-  }else if(colVal< 80){
-    col = '#3A7F00';
-  }else{  
-    col = '#1D4000';
+  if(colVal < 20){
+    col = cols[0];
+  }else if(colVal < 25){
+    col = cols[1];
+  }else if(colVal < 30){
+    col = cols[2];
+  }else if(colVal < 35){
+    col = cols[3];
+  }else if(colVal < 40){
+    col = cols[4];
+  }else if(colVal < 45){
+    col = cols[5];
+  }else{
+    col = cols[6];
   }
 
+  console.log(fills);
   //Make an SVG Container
   var svgContainer = d3.select("#trashDisplays").append("svg")
                                      .attr("width", 100)
-                                     .attr("height", 100)
-                                     .attr("padding", "20px 20px 20px 20px");
-
+                                     .attr("height", 150)
+                                     .attr("padding", "10px 10px 10px 10px");
     //Draw the Rectangle
   svgContainer.append("rect")
-                 .attr("width", 51)
+                 .attr("width", 50)
                  .attr("y", function(d){
-                  return 100 - parseFloat(fills[i]);
+                  return 110 - parseFloat(fills[i]);
                  })
                  .attr("height", function(d){
-                  return parseFloat(fills[i]);})
+                  return parseFloat(fills[i]) + 10;})
                  .attr("fill", col);
+
   
 
   svgContainer.append("text")
          .attr('class', 'TrashCan' + i)
                .attr('text-anchor', 'middle')
                .attr("x", 25)
-               .attr("y", 15)
-        .text('T.C.' + (i+1));
+               .attr("y", 10)
+        .text('#' + (i+1));
   }
