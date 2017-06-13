7aa0f9ddce448a1372299dbf02c8e859d19622f8
Herrick Fang <herrickfang@gmail.com>
2017-06-13 11:49:52 -0700

    added some d3 code

--- map/templates/map/map.js
+++ map/templates/map/map.js
@@ -19,3 +19,46 @@ function initMap() {
     });
   }
 }
+
+
+ // get the fills
+ var fills = [{% for bin in latest_trash_list %} "{{bin.fillLevel}}", {% endfor %}];
+
+ var i;
+ for(i = 0; i < fills.length; i++){
+  var col;
+  var colVal = 100 - parseFloat(fills[i]);
+  if(colVal < 40){
+    col = '#800000'
+  }else if(colVal< 60){
+    col = '#58BF00'; 
+  }else if(colVal< 80){
+    col = '#3A7F00';
+  }else{  
+    col = '#1D4000';
+  }
+
+  //Make an SVG Container
+  var svgContainer = d3.select("#trashDisplays").append("svg")
+                                     .attr("width", 100)
+                                     .attr("height", 100)
+                                     .attr("padding", "20px 20px 20px 20px");
+
+    //Draw the Rectangle
+  svgContainer.append("rect")
+                 .attr("width", 51)
+                 .attr("y", function(d){
+                  return 100 - parseFloat(fills[i]);
+                 })
+                 .attr("height", function(d){
+                  return parseFloat(fills[i]);})
+                 .attr("fill", col);
+  
+
+  svgContainer.append("text")
+         .attr('class', 'TrashCan' + i)
+               .attr('text-anchor', 'middle')
+               .attr("x", 25)
+               .attr("y", 15)
+        .text('T.C.' + (i+1));
+  }
--- map/urls.py
+++ map/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     url(r'(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$',
-    	views.DetailView.as_view(),
-    	name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
