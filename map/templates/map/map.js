d5da4b41ce76ff37a63077143d4412164604e1fd
Herrick Fang <herrickfang@gmail.com>
2017-06-13 15:14:51 -0700

    fixed the templating

--- map/templates/map/map.js
+++ map/templates/map/map.js
@@ -20,22 +20,14 @@ function initMap() {
     }
 }
 
-
-// get the fills
-var fills = [{% for bin in latest_trash_list %} "{{bin.fillLevel}}", {% endfor %}];
-fills = [];
-for(i=0; i < 40; i++){
-    fills.push(Math.random()*100);
-}
-var i;
-
+// Color gradient setup
 var cols = ['#BF0014','#c66100', '#ca9f00', '#ca9f00', '#bbce00', '#80d200', '#42d500', '#02d900']
-
 var count = 0;
 
 {% for bin in latest_trash_list %}
     count += 1;
-    var colVal = 100 - parseFloat(fills[count-1]);
+    // set up the color value
+    var colVal = 100 - parseFloat({{bin.fillLevel}});
     if(colVal < 20){
         col = cols[0];
     }else if(colVal < 25){
@@ -64,10 +56,10 @@ var count = 0;
         .attr("width", 50)
         .attr("x", 25)
         .attr("y", function(d){
-            return 110 - parseFloat(fills[count-1]);
+            return 110 - parseFloat({{bin.fillLevel}});
         })
         .attr("height", function(d){
-            return parseFloat(fills[count-1]) + 10;})
+            return parseFloat({{bin.fillLevel}}) + 10;})
         .attr("fill", col);
     //Append text
     svgContainer.append("text")
