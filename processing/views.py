4c6ea0ed130095c7ccff7b942475be2ef2edfec3
Herrick Fang <herrickfang@gmail.com>
2017-07-10 15:42:45 -0700

    contains the commented mixin version

--- processing/views.py
+++ processing/views.py
@@ -1,54 +1,73 @@
-from rest_framework import status
-# from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.http import Http404
-from rest_framework.views import APIView
+# from rest_framework import status
+# from rest_framework.response import Response
+# from django.http import Http404
+# from rest_framework.views import APIView
 from processing.models import TrashCan
 from processing.serializers import TrashCanSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
-class TrashList(APIView):
-    """
-    List all code trashcans, or create a new trashcan.
-    """
-    def get(self, request, format=None):
-        trashcans = TrashCan.objects.all()
-        serializer = TrashCanSerializer(trashcans, many=True)
-        return Response(serializer.data)
+class TrashList(generics.ListCreateAPIView):
+    queryset = TrashCan.objects.all()
+    serializer_class = TrashCanSerializer
 
-    def post(self, request, format=None):
-        serializer = TrashCanSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status = status.HTTP_400_BAD_REQUEST)
+class TrashDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TrashCan.objects.all()
+    serializer_class = TrashCanSerializer
 
-class TrashDetail(APIView):
-    """
-    Retrieve, update, or delete a snippet instance.
-    """
-    def get_object(self, pk):
-        try:
-            return TrashCan.objects.get(pk=pk)
-        except TrashCan.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        trashcan = self.get_object(pk)
-        serializer = TrashCanSerializer(trashcan)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        trashcan = self.get_object(pk)
-        serializer = TrashCanSerializer(trashcan, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        trashcan = self.get_object(pk)
-        trashcan.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#    def get(self, request, *args, **kwargs):
+#        return self.retrieve(request, *args, **kwargs)
+#
+#    def put(self, request, *args, **kwargs):
+#        return self.update(request, *args, **kwargs)
+#
+#    def delete(self, request, *args, **kwargs):
+#        return self.destroy(request, *args, **kwargs)
+#
+#
+# class TrashList(APIView):
+#     """
+#     List all code trashcans, or create a new trashcan.
+#     """
+#     def get(self, request, format=None):
+#         trashcans = TrashCan.objects.all()
+#         serializer = TrashCanSerializer(trashcans, many=True)
+#         return Response(serializer.data)
+# 
+#     def post(self, request, format=None):
+#         serializer = TrashCanSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.data, status = status.HTTP_400_BAD_REQUEST)
+# 
+# class TrashDetail(APIView):
+#     """
+#     Retrieve, update, or delete a snippet instance.
+#     """
+#     def get_object(self, pk):
+#         try:
+#             return TrashCan.objects.get(pk=pk)
+#         except TrashCan.DoesNotExist:
+#             raise Http404
+# 
+#     def get(self, request, pk, format=None):
+#         trashcan = self.get_object(pk)
+#         serializer = TrashCanSerializer(trashcan)
+#         return Response(serializer.data)
+# 
+#     def put(self, request, pk, format=None):
+#         trashcan = self.get_object(pk)
+#         serializer = TrashCanSerializer(trashcan, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# 
+#     def delete(self, request, pk, format=None):
+#         trashcan = self.get_object(pk)
+#         trashcan.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 # ##  do not do this is in the real test. 
