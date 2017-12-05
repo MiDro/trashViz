c624f1ed01471fb95698885ecc3ca55671ec5d08
CS70 cwhitsett <ewhitsett@hmc.edu>
2017-12-04 20:33:24 -0800

    Added twilio support

---
+++ processing/message.py
@@ -0,0 +1,13 @@
+import requests
+
+def send_message(msg):
+    token   = "6a6721e8785ce736651da2e9146515d8"
+    account = "ACe54aaa99ade5b73c877fa88c750c3cbd"
+    url     = "https://api.twilio.com/2010-04-01/Accounts/ACe54aaa99ade5b73c877fa88c750c3cbd/Messages"
+    
+    data = {
+        "From": "4158872366",
+        "To": "4153686487"
+    }
+    resp = requests.post(url, data=data, auth=(account, token))
+
